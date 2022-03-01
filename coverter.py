574349a3123096c0f09788306c139924880c2ed8
Jeevan Joseph <jeevanjoseph1310@gmail.com>
2022-03-01 09:01:30 +0530

    Third Commit

--- coverter.py
+++ coverter.py
@@ -9,31 +9,45 @@ def script():
     main=input("Enter the option for continuing(1/2/3) :")
     if main=='1':
         print("These are the type conversions available")
+        print("To start conversion make sure that you upload the requested image here at a time")
         print("1.png to jpg")
         print("2.jpg to png")
-        cn = input("Enter choice for types of coversion(1/2):")
+        print("3.jpeg to png")
+        cn = input("Enter choice for types of coversion(1/2/3):")
         if cn=='1':
             for file in glob.glob("*.png"):
-             print("Image selecetd is =",file)
-            choice = input("Enter choice for continuation(Y/N):")
-            if choice=='Y':
-                im = Image.open(file)
-                rgb_im = im.convert('RGB')
-                rgb_im.save(file.replace("png", "jpg"), quality=95)
-                print ("Executed")
-            else:
-                print("Thank you for using me")
+                print("Image selecetd is =",file)
+                choice = input("Enter choice for continuation(Y/N):")
+                if choice=='Y':
+                    im = Image.open(file)
+                    rgb_im = im.convert('RGB')
+                    rgb_im.save(file.replace("png", "jpg"), quality=95)
+                    print ("Executed")
+                else:
+                    print("Thank you for using me")
         elif cn=='2':
             for file in glob.glob("*.jpg"):
-             print("Image selecetd is =",file)
-            choice = input("Enter choice for continuation(Y/N):")
-            if choice=='Y':
-                im = Image.open(file)
-                rgb_im = im.convert('RGB')
-                rgb_im.save(file.replace("jpg", "png"), quality=95)
-                print ("Executed")
-            else:
-                print("Thank your for using me")
+                print("Image selecetd is =",file)
+                choice = input("Enter choice for continuation(Y/N):")
+                if choice=='Y':
+                    im = Image.open(file)
+                    rgb_im = im.convert('RGB')
+                    rgb_im.save(file.replace("jpg", "png"), quality=95)
+                    print ("Executed")
+                else:
+                    print("Thank your for using me")
+        elif cn=='3':
+            for file in glob.glob("*.jpeg"):
+                print("Image selected is =",file)
+                choice=input("Enter the choice for continuation(Y/N):")
+                if choice=='Y':
+                    im=Image.open(file)
+                    rgb_im = im.convert('RGB')
+                    rgb_im.save(file.replace("jpeg","png"),quality=95)
+                    print ("Executed")
+                else:
+                    print("Thank You for using me")
+
         else:
             print("Error occured")
     elif main=='2':
@@ -42,32 +56,32 @@ def script():
         re = input("Enter choice for types of coversion(1/2):")
         if re=='1':
             for file in glob.glob("*.png"):
-             print("Image selecetd is =",file)
-            choice = input("Enter choice for continuation(Y/N):")
-            if choice=='Y':
-                im = Image.open(file)
-                w=int(input("enter the width :"))
-                h=int(input("enter the height :"))
-                resized_img = im.resize((w,h))
-                resized_img.save("copy.png")
-                print("executed")
-            else:
-                print("Thank You")
+                print("Image selecetd is =",file)
+                choice = input("Enter choice for continuation(Y/N):")
+                if choice=='Y':
+                    im = Image.open(file)
+                    w=int(input("enter the width :"))
+                    h=int(input("enter the height :"))
+                    resized_img = im.resize((w,h))
+                    resized_img.save("copy.png")
+                    print("executed")
+                else:
+                    print("Thank You")
         elif re=='2':
             for file in glob.glob("*.jpg"):
-             print("Image selecetd is =",file)
-            choice = input("Enter choice for continuation(Y/N):")
-            if choice=='Y':
-                im = Image.open(file)
-                w=int(input("enter the width :"))
-                h=int(input("enter the height :"))
-                resized_img = im.resize((w,h))
-                resized_img.save("copy.jpg")
-                print("executed")
-            else:
-                print("Thank you")
+                print("Image selecetd is =",file)
+                choice = input("Enter choice for continuation(Y/N):")
+                if choice=='Y':
+                    im = Image.open(file)
+                    w=int(input("enter the width :"))
+                    h=int(input("enter the height :"))
+                    resized_img = im.resize((w,h))
+                    resized_img.save("copy.jpg")
+                    print("executed")
+                else:
+                    print("Thank you")
         else:
-         print("Undefined variable used")
+            print("Undefined variable used")
     elif main=='3':
         print("Help is here")
         print("use this resizer wisely")
